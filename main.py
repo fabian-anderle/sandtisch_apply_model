ce9539212ae95e41739c84f953d9743e152f0d19
ep1czoul <59176094+ep1czoul@users.noreply.github.com>
2023-02-19 15:44:58 +0100

    update, refactor

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import numpy as np
 import tensorflow as tf
 import onnx
 from onnx_tf.backend import prepare
-#from PIL import Image
-from IPython.display import display
 from matplotlib import pyplot as plt
 
+output_img_name = "output.jpg"
 input_img_name = "input.jpg"
 width, height = 256, 256
 models = {
@@ -46,21 +45,23 @@ def convert_data_onnx(img):
     tensor = tf.expand_dims(tensor, axis=0)
     return [{'x': tensor}]
 
-def show_img(img, title:str, wait_key = 0):
-    cv2.imshow(title, img)
-    cv2.waitKey(wait_key)
-    cv2.destroyAllWindows()
-
 def plot_images_from_dict(image_dict):
     num_images = len(image_dict)
     fig, axs = plt.subplots(1, num_images, figsize=(5 * num_images, 5))
 
     for i, (title, image) in enumerate(image_dict.items()):
-        axs[i].imshow(image)
+        axs[i].imshow(image * 0.5 + 0.5)
         axs[i].set_title(title)
         axs[i].axis('off')
 
     plt.show()
+
+def save_output_img(img, name):
+    cv2.imwrite(name, img)
+
+def adjust_channels(img):
+    img_array = np.array(img * 127.5 + 127.5)
+    return cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
 
 if __name__ == '__main__':
     img_depth = import_img(input_img_name)
@@ -68,22 +69,12 @@ if __name__ == '__main__':
     img_depth_converted = convert_data_tf(img_depth)
     img_sat = gen_image_tf(model, img_depth_converted)
 
-    display_list = [img_depth_converted[0], img_sat[0]]
-    title = ['Input Image', 'Output Image']
     display_imgs = {
         'Input Image Processed': img_depth_converted[0],
         'Output Image': img_sat[0]
     }
 
     plot_images_from_dict(display_imgs)
-
-    for i in range(2):
-        plt.subplot(1, 2, i+1)
-        plt.title(title[i])
-        # Getting the pixel values in the [0, 1] range to plot.
-        plt.imshow(display_list[i] * 0.5 + 0.5)
-        plt.axis('off')
-    plt.show()
-
+    save_output_img(adjust_channels(img_sat[0]), output_img_name)
 
 
